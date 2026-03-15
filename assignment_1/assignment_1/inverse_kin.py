527e2f4b8066590141e46db7a0186a571c694a8f
Daehyung Park <pidipidi52@gmail.com>
2026-03-15 10:10:21 +0900

    fixed import errors

--- assignment_1/assignment_1/inverse_kin.py
+++ assignment_1/assignment_1/inverse_kin.py
@@ -20,7 +20,7 @@ from trajectory_msgs.msg import JointTrajectoryPoint
 from builtin_interfaces.msg import Duration
 
 import numpy as np
-from assignment_2 import move_joint
+from assignment_1 import move_joint
 
 JOINT_NAMES = ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint',
                'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint']
